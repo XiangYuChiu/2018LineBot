8597234c74288800fa3f78475784af1fcac7c071
XiangYuChiu <61840928+XiangYuChiu@users.noreply.github.com>
2020-03-27 01:35:57 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,19 @@
 from flask import Flask, request, abort
 
-
-
 from linebot import (
     LineBotApi, WebhookHandler
 )
-
 from linebot.exceptions import (
     InvalidSignatureError
 )
-
 from linebot.models import *
 
 app = Flask(__name__)
 
 # Channel Access Token
-line_bot_api = 'hXgcnkaAxXj4+xTcmEOZCVrU8Pwk5kS9qiB6gJX7msIlOYJkwTO4bFzh2lgFgbdnROSB3apQGarvQS6REB+JDANY1CUuL82RFEORBleo1Ogi3OJzsQZSK8fhTZIr//BQ8CVSdxrT25otDcm/hqsVBgdB04t89/1O/w1cDnyilFU='
+line_bot_api = LineBotApi('hXgcnkaAxXj4+xTcmEOZCVrU8Pwk5kS9qiB6gJX7msIlOYJkwTO4bFzh2lgFgbdnROSB3apQGarvQS6REB+JDANY1CUuL82RFEORBleo1Ogi3OJzsQZSK8fhTZIr//BQ8CVSdxrT25otDcm/hqsVBgdB04t89/1O/w1cDnyilFU=')
 # Channel Secret
-handler ='338644819138a86bd22b88b740255b03'
+handler = WebhookHandler('338644819138a86bd22b88b740255b03')
 
 # 監聽所有來自 /callback 的 Post Request
 @app.route("/callback", methods=['POST'])
@@ -25,7 +21,6 @@ def callback():
     # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
     # get request body as text
-
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
     # handle webhook body
@@ -38,16 +33,11 @@ def callback():
 # 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    get = event.message.text
-#event.gessage.text接收使用者文字訊息
-    mssage = TextSendMessage(text = '怕.jpg')
-
-#############################################
-
+    message = TextSendMessage(text=event.message.text)
     line_bot_api.reply_message(event.reply_token, message)
 
-
-
 if __name__ == "__main__":
-
     app.run()
+
+
+
