f4a5b93f193b57ee8b47654e994d1bc7e021d3ce
XiangYuChiu <61840928+XiangYuChiu@users.noreply.github.com>
2020-03-27 12:55:42 +0800

    Update app.py

--- app.py
+++ app.py
@@ -33,10 +33,10 @@ def callback():
 # 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    if message == ('123'):
+    if (event.message.text = '123'):
         message == ('234')
-    message = TextSendMessage(text=event.message.text)
-    line_bot_api.reply_message(event.reply_token, message)
+    replymessage = TextSendMessage(text=message)
+    line_bot_api.reply_message(event.reply_token, replymessage)
 
 if __name__ == "__main__":
     app.run()
