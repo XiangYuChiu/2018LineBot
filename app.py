42f6ed623ce18c5e9b2d7d3d2ea71735f82be44f
XiangYuChiu <61840928+XiangYuChiu@users.noreply.github.com>
2020-03-30 18:39:32 +0800

    Update app.py

--- app.py
+++ app.py
@@ -35,19 +35,19 @@ def callback():
 def handle_message(event):
     if (event.message.text == '123'):   #獲取測試訊息
         replymessage = ('234')          #設定回復訊息(replymessage)
-        share_message
     elif(event.message.text == 'Wifi'): 
         replymessage = (' ASUS-RT51U: \n Password: d0645758\n\n dlink-F392:\n Password: 51232960\n\n ASUS_Zenfone3:\n Password: D0645758\n') 
-        share_message
     elif(event.message.text == '生存場地'):
         counter = 0
         while(counter<=2):
             if(counter == 1):
                 replymessage = ('各場地資料彙整\n場地地圖 https://goo.gl/dV8xwq\n各場地資料彙整如下\n\n')
-                share_message
+                message = TextSendMessage(text=replymessage)               #將回復訊息(replymessage)輸入LINE BOT(message)
+                line_bot_api.reply_message(event.reply_token, message)     #LINE BOT回復訊息
             elif(counter == 2):
                 replymessage = ('山湖戰術中心\n官網:  https://www.facebook.com/hillfoxtacticsCenter/\n導航直接搜尋 山狐戰術中心\n收費場 半天200/人 全天300/人')
-                share_message
+                message = TextSendMessage(text=replymessage)               #將回復訊息(replymessage)輸入LINE BOT(message)
+                line_bot_api.reply_message(event.reply_token, message)     #LINE BOT回復訊息
         #溪湖戰役\n官網:  https://www.facebook.com/XIHUAirsoftWar/\nGoogle 導航溪湖戰役即可\n收費場 入場費平日200/人 假日300/人 \n有販賣飲料/出租裝備\n\n
         #烏日橋下\n地址:  https://goo.gl/maps/6eFPrSx4AU42\n橋下鐵絲網後就是活動場地 為免費場地\n可走筏子溪東街或新鎮和路再轉至橋下\n請禮讓偶爾出現的工程車在那邊施工\n\n
         #春田惡魔島\n官網:  https://goo.gl/E4gPqY\n導航” 春田惡魔島” 或東山樂園再往上一點\n收費場 250/人\n有販賣簡單的飲料\n\n
@@ -57,9 +57,7 @@ def handle_message(event):
         #JEA共同決戰區生存遊戲訓練基地\n網址 https://www.facebook.com/jea.shootingclub/\n地址 苗栗縣後龍鎮龍坑里十班坑172號\n收費場 半天200/人 有提供裝備出借\n\n
         #南投千秋戰場\n網址:  https://www.facebook.com/groups/299470600702193/?fref=nf\n地址;  南投市千秋里菓稟路3號\n收費場 100/人/半天\n\n
             counter += 1
-            share_message
             
-def share_message(replymessage):
     message = TextSendMessage(text=replymessage)               #將回復訊息(replymessage)輸入LINE BOT(message)
     line_bot_api.reply_message(event.reply_token, message)     #LINE BOT回復訊息
 #######################################################################
