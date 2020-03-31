f5702cc91d78d4cca4fdf5cb801b408d11d0f356
XiangYuChiu <61840928+XiangYuChiu@users.noreply.github.com>
2020-03-31 11:57:58 +0800

    Update app.py

--- app.py
+++ app.py
@@ -70,7 +70,7 @@ def handle_message(event):
         replymessage = TemplateSendMessage(
             alt_text = '商502查詢(請用手機操作)',  # 當你發送到你的Line bot 群組的時候，通知的名稱
             template = ButtonsTemplate(
-                thumbnail_image_url = 'https://drive.google.com/file/d/1xtSBD_qtV5p1Ka7Lacf0qBstfEnouam6/view?usp=sharing',  # 你的按鈕模板的圖片是什麼
+                thumbnail_image_url = 'https://i.imgur.com/6Rj7CIx.jpg',  # 你的按鈕模板的圖片是什麼
                 title = '商502',  # 你的標題名稱
                 text = '請選擇你要的項目：',  # 應該算是一個副標題
                 # 下面主要就是你希望使用者點擊了按鈕會有哪些動作，最多只能有四個action！超過會報錯喔！
