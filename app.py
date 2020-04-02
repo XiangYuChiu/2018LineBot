63fb84d4897f9d10d5188e24e2775c4b0840fc75
XiangYuChiu <61840928+XiangYuChiu@users.noreply.github.com>
2020-04-02 23:19:20 +0800

    Update app.py

--- app.py
+++ app.py
@@ -57,7 +57,7 @@ def handle_message(event):
         reply_arr.append(TextSendMessage(text='翔準生存遊戲專賣\n地址:508彰化縣和美鎮彰新路二段479號\n營業時間:12:00~22:00\n電話:04-7353481\n官網:https://www.aog.com.tw/\n露天賣場1:https://www.ruten.com.tw/user/index00.php?s=zzzlin8899\n露天賣場2:https://www.ruten.com.tw/user/index00.php?s=and888999\n\nBCS武器空間生存遊戲專賣 台中美村店\n地址:403台中市西區美村路一段109號\n營業時間:13:00~22:00\n電話:04-23269483\n露天:https://www.ruten.com.tw/user/index00.php?s=bcsbcs\n\nBCS武器空間生存遊戲專賣-台中NOVA英才店\n地址:403台中市西區英才路508號2號2樓241櫃位\n營業時間:11:00~21:30\n電話:04-23293192\n\n金和勝玩具-西屯店\n地址:407台中市西屯區西屯路三段79-8號\n營業時間14:00~22:00\n電話:04-24511795\n露天:https://www.ruten.com.tw/user/index00.php?s=a400258\n\n金和勝玩具-彰化店\n地址:500彰化縣彰化市中央路9號\n電話:04-7621300\n\n'))
         reply_arr.append(TextSendMessage(text='BCS武器空間生存遊戲專賣 台中美村店\n地址:403台中市西區美村路一段109號\n營業時間:13:00~22:00\n電話:04-23269483\n露天:https://www.ruten.com.tw/user/index00.php?s=bcsbcs\n\nBCS武器空間生存遊戲專賣-台中NOVA英才店\n地址:403台中市西區英才路508號2號2樓241櫃位\n營業時間:11:00~21:30\n電話:04-23293192\n\n'))
         reply_arr.append(TextSendMessage(text='金和勝玩具-西屯店\n地址:407台中市西屯區西屯路三段79-8號\n營業時間14:00~22:00\n電話:04-24511795\n露天:https://www.ruten.com.tw/user/index00.php?s=a400258\n\n金和勝玩具-彰化店\n地址:500彰化縣彰化市中央路9號\n電話:04-7621300\n\n'))
-    elif(event.message.text == '推廣'):
+    elif(event.message.text == '推廣','推廣首頁'):
         reply_arr.append(TemplateSendMessage(
             alt_text='推廣教室查詢(請用手機操作)',
             template=ConfirmTemplate(
@@ -102,7 +102,36 @@ def handle_message(event):
                 ]
             )
         ))
-        
+    elif(event.message.text == '商502導師機'):
+        reply_arr.append(TemplateSendMessage(
+            alt_text = '商502導師機查詢(請用手機操作)',  # 當你發送到你的Line bot 群組的時候，通知的名稱
+            template = ButtonsTemplate(
+                thumbnail_image_url = 'https://i.imgur.com/TBcAqsh.jpg',  # 你的按鈕模板的圖片是什麼
+                title = '商502導師機查詢',  # 你的標題名稱
+                text = '請選擇你要的項目：',  # 應該算是一個副標題
+                # 下面主要就是你希望使用者點擊了按鈕會有哪些動作，最多只能有四個action！超過會報錯喔！
+                actions = [
+                    # 說真的這個我不知道要幹嘛用，可能後台可以收數據？我點了就回應我 postback text，至於data我就不熟了
+                    PostbackAction(
+                        label = '程式項目',  # 在按鈕模板上顯示的名稱
+                        text = '商502導師機程式項目',  # 點擊會顯示的文字
+                        data = 'action=buy&itemid=1'  # 這個...我真的就不知道了～
+                    ),
+                    # 跟上面差不多
+                    MessageAction(
+                        label = '系統資訊',   # 在按鈕模板上顯示的名稱
+                        text = '商502導師機系統資訊',  # 點擊會顯示的文字
+                    ),
+                    MessageAction(
+                        label = '故障資訊',   # 在按鈕模板上顯示的名稱
+                        text = '商502導師機故障資訊',  # 點擊會顯示的文字
+                    )
+                    MessageAction(
+                        label = '回到推廣首頁',
+                        text = '推廣首頁',
+                ]
+            )
+        ))
     line_bot_api.reply_message(event.reply_token,reply_arr)     #LINE BOT回復訊息
 #######################################################################
 if __name__ == "__main__":
