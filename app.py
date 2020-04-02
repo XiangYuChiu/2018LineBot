da9e05f2e1aa20ae297897cc7ec9ad885d94d519
XiangYuChiu <61840928+XiangYuChiu@users.noreply.github.com>
2020-04-03 00:00:29 +0800

    Update app.py

--- app.py
+++ app.py
@@ -142,7 +142,37 @@ def handle_message(event):
                 ]
             )
         ))
-     
+    elif(event.message.text == '商502學生機'):
+        reply_arr.append(TemplateSendMessage(
+            alt_text = '商502學生機查詢(請用手機操作)',  # 當你發送到你的Line bot 群組的時候，通知的名稱
+            template = ButtonsTemplate(
+                thumbnail_image_url = 'https://i.imgur.com/lh0w50B.jpg',  # 你的按鈕模板的圖片是什麼
+                title = '商502學生機查詢',  # 你的標題名稱
+                text = '請選擇你要的項目：',  # 應該算是一個副標題
+                # 下面主要就是你希望使用者點擊了按鈕會有哪些動作，最多只能有四個action！超過會報錯喔！
+                actions = [
+                    # 說真的這個我不知道要幹嘛用，可能後台可以收數據？我點了就回應我 postback text，至於data我就不熟了
+                    PostbackAction(
+                        label = '程式項目',  # 在按鈕模板上顯示的名稱
+                        text = '商502學生機程式項目',  # 點擊會顯示的文字
+                        data = 'action=buy&itemid=1'  # 這個...我真的就不知道了～
+                    ),
+                    # 跟上面差不多
+                    MessageAction(
+                        label = '系統資訊',   # 在按鈕模板上顯示的名稱
+                        text = '商502學生機系統資訊',  # 點擊會顯示的文字
+                    ),
+                    MessageAction(
+                        label = '故障資訊',   # 在按鈕模板上顯示的名稱
+                        text = '商502學生機故障資訊',  # 點擊會顯示的文字
+                    ),
+                    MessageAction(
+                        label = '回到推廣首頁',
+                        text = '推廣',
+                    )
+                ]
+            )
+        )) 
     elif(event.message.text == '3D列印機'):
         reply_arr.append(TemplateSendMessage(
             alt_text = '3D列印機查詢(請用手機操作)',  # 當你發送到你的Line bot 群組的時候，通知的名稱
