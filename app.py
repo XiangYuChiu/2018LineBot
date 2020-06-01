13fea720144b55233a22bfc1f5f04979c1f43ad7
XiangYuChiu <61840928+XiangYuChiu@users.noreply.github.com>
2020-06-02 01:50:12 +0800

    Update app.py

--- app.py
+++ app.py
@@ -82,33 +82,46 @@ def handle_message(event):
     elif(event.message.text == '秘笈來也'):
         reply_arr.append(TemplateSendMessage(
             alt_text = '秘笈來也(請用手機操作)',  # 當你發送到你的Line bot 群組的時候，通知的名稱
-            template = ButtonsTemplate(
-                thumbnail_image_url = 'https://i.imgur.com/AwaTv9O.jpg',  # 你的按鈕模板的圖片是什麼
-                title = '武功秘笈',  # 你的標題名稱
-                text = '以下是目前的相關檔案：',  # 應該算是一個副標題
-                # 下面主要就是你希望使用者點擊了按鈕會有哪些動作，最多只能有四個action！超過會報錯喔！
-                actions = [
-                 # 說真的這個我不知道要幹嘛用，可能後台可以收數據？我點了就回應我 postback text，至於data我就不熟了
-                    PostbackTemplateAction(
-                        label = 'MicosoftTeams 2020.5',  # 在按鈕模板上顯示的名稱
-                        text = 'Micosoft Teams',  # 點擊會顯示的文字
-                    ),
-                    # 跟上面差不多
-                    MessageTemplateAction(
-                        label = '3D列印機說明書 廠商提供',   # 在按鈕模板上顯示的名稱
-                        text = '3D列印機說明書',  # 點擊會顯示的文字
-                    ),
-                    MessageTemplateAction(
-                        label = '還原系統開啟 2019.10',   # 在按鈕模板上顯示的名稱
-                        text = 'CPR還原系統開啟方法',  # 點擊會顯示的文字
-                    ),
-                    MessageTemplateAction(
-                        label = '各項軟體版本號',
-                        text = '502各項軟體版本號',
+            template =CarouselTemplate(
+                columns=[
+                    CarouselColumn(
+                        thumbnail_image_url = 'https://i.imgur.com/AwaTv9O.jpg',  # 你的按鈕模板的圖片是什麼
+                        title = '武功秘笈',  # 你的標題名稱
+                        text = '以下是目前的相關檔案：',  # 應該算是一個副標題# 下面主要就是你希望使用者點擊了按鈕會有哪些動作，最多只能有四個action！超過會報錯喔！
+                        actions = [# 說真的這個我不知道要幹嘛用，可能後台可以收數據？我點了就回應我 postback text，至於data我就不熟了
+                            MessageTemplateAction(
+                                label = 'MicosoftTeams 2020.5',  # 在按鈕模板上顯示的名稱
+                                text = 'Micosoft Teams',  # 點擊會顯示的文字
+                            ),
+                            # 跟上面差不多
+                            MessageTemplateAction(
+                                label = '還原系統開啟 2019.10',   # 在按鈕模板上顯示的名稱
+                                text = 'CPR還原系統開啟方法',  # 點擊會顯示的文字
+                            ),
+                            MessageTemplateAction(
+                                label = '3D列印機說明書 廠商提供',   # 在按鈕模板上顯示的名稱
+                                text = '3D列印機說明書',  # 點擊會顯示的文字
+                            )
+                        ]
                     )
-                ]
-            )
-        ))
+                    CarouselColumn(
+                        thumbnail_image_url = 'https://i.imgur.com/AwaTv9O.jpg',  # 你的按鈕模板的圖片是什麼
+                        title = '武功秘笈',  # 你的標題名稱
+                        text = '以下是目前的相關檔案：',  # 應該算是一個副標題# 下面主要就是你希望使用者點擊了按鈕會有哪些動作，最多只能有四個action！超過會報錯喔！
+                        actions = [# 說真的這個我不知道要幹嘛用，可能後台可以收數據？我點了就回應我 postback text，至於data我就不熟了
+                            MessageTemplateAction(
+                                label = '各項軟體版本號',
+                                text = '502各項軟體版本號',
+                            ),
+                            MessageTemplateAction(
+                                label = 'SoildWorks檔案路徑',  # 在按鈕模板上顯示的名稱
+                                text = 'SoildWorks檔案路徑',  # 點擊會顯示的文字
+                            )
+                        ]
+                    )
+                    ]
+                    )
+                ))
         '''reply_arr.append(TemplateSendMessage(
             template = ButtonsTemplate(
             thumbnail_image_url = 'https://i.imgur.com/AwaTv9O.jpg',  # 你的按鈕模板的圖片是什麼
